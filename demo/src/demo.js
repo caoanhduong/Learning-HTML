86ed0ffeac4ce433bcaac7d28684abd0ff77a195
caduong <anhduongtk97@gmail.com>
2019-07-21 21:08:12 +0700

    update

---
+++ demo/src/demo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Demo() {
+  return (
+    <div className="Demo">
+      <h1>Hello ReactJS</h1>
+      <p>Chào mừng bạn đến với khóa học</p>
+    </div>
+  );
+}
+
+export default Demo
+;
